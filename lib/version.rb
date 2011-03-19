9453685aea016ad0214e9f09e051bd1e76eb7e59
arvicco <arvitallian@gmail.com>
2011-03-19 18:22:24 +0300

    Importing P2ClientGate dir

--- lib/version.rb
+++
@@ -1,8 +0,0 @@
-require 'pathname'
-
-module P2
-
-  VERSION_FILE = Pathname.new(__FILE__).dirname + '../VERSION'   # :nodoc:
-  VERSION = VERSION_FILE.exist? ? VERSION_FILE.read.strip : nil
-
-end
--- spec/p2_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-module P2Test
-  describe P2 do
-  end
-end # module P2Test
-
